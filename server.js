339e9ae3ec19865aea3b7a73a74acf43ae8c7d8b
Gustavo Lopez <gustavo@getkalto.com>
2024-03-17 17:27:34 -0300

    Type module was deleted

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express from "express";
+// import express from "express";
+const express = require("express");
 
 const app = express();
-const __dirname = new URL(import.meta.url).pathname;
+// const __dirname = new URL(import.meta.url).pathname;
 
 app.use(express.static("public"));
 app.set("view engine", "pug");
